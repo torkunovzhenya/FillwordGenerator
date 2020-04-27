36b0e246f042bfd92fd2be61bda7c952e54656af
Zhenya Torkunov <ztorkunov@gmail.com>
2020-04-28 02:11:53 +0300

    Ended generation of polyminos

--- PolyminoGenerator/main.cpp
+++ PolyminoGenerator/main.cpp
@@ -1,28 +1,224 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-int minx, miny, maxx, maxy;
-
-void findWays(int x, int y, int left, string way, vector<vector<char>>& field)
+struct f
 {
+    int orientations = 0;
+    vector<pair<int, int>> cords;
+
+    friend bool operator==(const f& left, const f& right) { return left.cords == right.cords; }
+
+    friend bool operator!=(const f& left, const f& right) { return left.cords != right.cords; }
+
+    friend bool operator<(const f& left, const f& right) { return left.cords < right.cords; }
+
+    friend bool operator>(const f& left, const f& right) { return left.cords > right.cords; }
+};
+
+
+vector<vector<char>> field;
+set<f> figures_set;
+
+
+void dfs(int x, int y, int x1, int y1, int x2, int y2)
+{
+    if (x > x1 && !field[x - 1][y])
+    {
+        field[x - 1][y] += 2;
+        dfs(x - 1, y, x1, y1, x2, y2);
+    }
+
+    if (x < x2 && !field[x + 1][y])
+    {
+        field[x + 1][y] += 2;
+        dfs(x + 1, y, x1, y1, x2, y2);
+    }
+
+    if (y > y1 && !field[x][y - 1])
+    {
+        field[x][y - 1] += 2;
+        dfs(x, y - 1, x1, y1, x2, y2);
+    }
+
+    if (y < y2 && !field[x][y + 1])
+    {
+        field[x][y + 1] += 2;
+        dfs(x, y + 1, x1, y1, x2, y2);
+    }
+}
+
+
+// Function for checking if the figure contains empty places inside
+bool Count_for_loops(int x1, int y1, int x2, int y2)
+{
+    dfs(x1, y1, x1, y1, x2, y2);
+
+    int counter1 = 0;
+    int counter2 = 0;
+    for (int i = x1; i <= x2; ++i)
+    {
+        for (int j = y1; j <= y2; ++j)
+        {
+            if (!field[i][j])
+                return false;
+
+            if (field[i][j] == 2)
+                field[i][j] = 0;
+        }
+    }
+
+    return true;
+}
+
+
+void Add_to_set(int minx, int miny, int maxx, int maxy)
+{
+    if (!Count_for_loops(minx - 1, miny - 1, maxx + 1, maxy + 1))
+        return;
+
+    // Main position
+    f f1 = f();
+    for (int i = minx; i <= maxx; ++i)
+        for (int j = miny; j <= maxy; ++j)
+            if (field[i][j])
+                f1.cords.emplace_back(i - minx, j - miny);
+
+    f1.orientations = 1;
+
+    if (figures_set.find(f1) != figures_set.end())
+        return;
+
+    // Rotate 90 degrees clockwise
+    f f2 = f();
+    for (int j = miny; j <= maxy; ++j)
+        for (int i = maxx; i >= minx; --i)
+            if (field[i][j])
+                f2.cords.emplace_back(j - miny, maxx - i);
+
+    // If they are equal - all rotates will be the same
+    if (f2 != f1)
+    {
+        f1.orientations *= 2;
+
+        if (figures_set.find(f2) != figures_set.end())
+            return;
+
+        // Rotate 180 degrees clockwise
+        f f3 = f();
+        for (int i = maxx; i >= minx; --i)
+            for (int j = maxy; j >= miny; --j)
+                if (field[i][j])
+                    f3.cords.emplace_back(maxx - i, maxy - j);
+
+        // If they are equal - there are 2 pairs of same orientations
+        if (f3 != f1)
+        {
+            f1.orientations *= 2;
+
+            if (figures_set.find(f3) != figures_set.end())
+                return;
+
+            // Rotate 270 degrees clockwise
+            f f4 = f();
+            for (int j = maxy; j >= miny; --j)
+                for (int i = minx; i <= maxx; ++i)
+                    if (field[i][j])
+                        f4.cords.emplace_back(maxy - j, i - minx);
+
+            if (figures_set.find(f4) != figures_set.end())
+                return;
+        }
+    }
+
+    // Reflection main position
+    f f5 = f();
+    for (int i = minx; i <= maxx; ++i)
+        for (int j = maxy; j >= miny; --j)
+            if (field[i][j])
+                f5.cords.emplace_back(i - minx, maxy - j);
+
+    // If it is equal to Main position - skip other positions
+    if (f5 != f1)
+    {
+        if (figures_set.find(f5) != figures_set.end())
+            return;
+
+        if (f1.orientations != 1)
+        {
+            // Reflection rotate 90 degrees clockwise
+            f f6 = f();
+            for (int j = maxy; j >= miny; --j)
+                for (int i = maxx; i >= minx; --i)
+                    if (field[i][j])
+                        f6.cords.emplace_back(maxy - j, maxx - i);
+
+            if (f6 != f1)
+            {
+                if (figures_set.find(f6) != figures_set.end())
+                    return;
+
+
+                if (f1.orientations != 2)
+                {
+                    // Reflection rotate 180 degrees clockwise
+                    f f7 = f();
+                    for (int i = maxx; i >= minx; --i)
+                        for (int j = miny; j <= maxy; ++j)
+                            if (field[i][j])
+                                f7.cords.emplace_back(maxx - i, j - miny);
+
+                    if (figures_set.find(f7) != figures_set.end())
+                        return;
+
+                    // Reflection rotate 270 degrees clockwise
+                    f f8 = f();
+                    for (int j = miny; j <= maxy; ++j)
+                        for (int i = minx; i <= maxx; ++i)
+                            if (field[i][j])
+                                f8.cords.emplace_back(j - miny, i - minx);
+
+                    if (figures_set.find(f8) != figures_set.end())
+                        return;
+                }
+            }
+        }
+    }
+
+    for (int i = minx; i <= maxx; ++i)
+    {
+        for (int j = miny; j <= maxy; ++j)
+            cout << (int)field[i][j] << " ";
+        cout << endl;
+    }
+    figures_set.insert(f1);
+}
+
+
+void findWays(int x, int y, int left, string way, int minx, int miny, int maxx, int maxy)
+{
+    field[x][y] = 1;
+
     if (left == 0)
     {
-        cout << way << endl;
+        cout << way << " " << maxx - minx + 1 << " " << maxy - miny + 1 << endl;
+
+        Add_to_set(minx, miny, maxx, maxy);
+
+        field[x][y] = 0;
         return;
     }
 
-    field[x][y] = 1;
-
     if (!field[x - 1][y])
-        findWays(x - 1, y, left - 1, way + '^', field);
+        findWays(x - 1, y, left - 1, way + '^', min(x - 1, minx), miny, maxx, maxy);
     if (!field[x + 1][y])
-        findWays(x + 1, y, left - 1, way + 'v', field);
+        findWays(x + 1, y, left - 1, way + 'v', minx, miny, max(x + 1, maxx), maxy);
     if (!field[x][y - 1])
-        findWays(x, y - 1, left - 1, way + '<', field);
+        findWays(x, y - 1, left - 1, way + '<', minx, min(y - 1, miny), maxx, maxy);
     if (!field[x][y + 1])
-        findWays(x, y + 1, left - 1, way + '>', field);
+        findWays(x, y + 1, left - 1, way + '>', minx, miny, maxx, max(y + 1, maxy));
 
     field[x][y] = 0;
 }
@@ -42,14 +238,22 @@ int main(int argc, char* argv[]) {
     else
         n = stoi(argv[1]);
 
-    vector<vector<char>> field = vector<vector<char>>(2 * n, vector<char>(2 * n));
+    field = vector<vector<char>>(2 * n, vector<char>(2 * n));
     int x = n - 1;
     int y = n;
 
     field[n][n] = 1;
     field[x][y] = 1;
 
-    findWays(x, y, n - 2, "^", field);
+    findWays(x, y, n - 2, "^^", x, y, n, n);
+
+    for (f f : figures_set)
+    {
+        cout << f.orientations << endl;
+        for (auto cord : f.cords)
+            cout << "(" << cord.first << ":" << cord.second << ")";
+        cout << endl;
+    }
 
     return 0;
 }
